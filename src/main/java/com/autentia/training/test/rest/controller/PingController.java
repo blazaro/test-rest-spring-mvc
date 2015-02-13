2e5104006f9aa344e9ee17735167fc8d14e10ca2
Borja Lazaro <blazaro@autentia.com>
2015-02-13 09:23:59 +0100

    Cambiamos a la anotación @RestController que nos unifica la @Controller con la @ResponseBody.

--- src/main/java/com/autentia/training/test/rest/controller/PingController.java
+++ src/main/java/com/autentia/training/test/rest/controller/PingController.java
@@ -3,22 +3,20 @@ package com.autentia.training.test.rest.controller;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.autentia.training.test.rest.model.PingResponse;
 
-@Controller
+@RestController
 @RequestMapping("/v1/ping")
 public class PingController {
 
 	@RequestMapping(method = {RequestMethod.GET,RequestMethod.POST})
 	@ResponseStatus(value = HttpStatus.OK)
-	@ResponseBody
 	public PingResponse ping(@RequestParam(value = "input", required = true) String input) {
 		return new PingResponse(new Date(), input);
 	}
